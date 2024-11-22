3face5008e7c610bc7bafc29b8153c5e5654ef6d
onlinewithbeta <onlinewithbeta@gmail.com>
2024-11-22 03:25:40 +0100

    update tips with event listener

--- Tips.js
+++ Tips.js
@@ -1,9 +1,9 @@
 const tips = [
   { content: "Use active learning strategies to enhance your study effectiveness!", author: "—Unknown" },
-  { content: "Break your study sessions into smaller, manageable chunks.", author: "— John Doe" },
-  { content: "Stay organized with a planner to track assignments and deadlines.", author: "— Jane Smith" },
-  { content: "Prioritize tasks based on deadlines and importance.", author: "— Mary Johnson" },
-  { content: "Practice mindfulness techniques to reduce stress during exams.", author: "— David Brown" }
+  { content: "Break your study sessions into smaller, manageable chunks.", author: "John Doe" },
+  { content: "Stay organized with a planner to track assignments and deadlines.", author: "Jane Smith" },
+  { content: "Prioritize tasks based on deadlines and importance.", author: "Mary Johnson" },
+  { content: "Practice mindfulness techniques to reduce stress during exams.", author: "David Brown" }
         ];
 
 //function to display tips
@@ -14,3 +14,7 @@ function displayTips() {
   Id('tipAuthor').innerText = `~${current_Tip.Author}`;
   Id('tipContent').innerText = current_Tip.Quoute;
 }
+
+document.getElementById('nextTip').addEventListener('click',()=>{
+  displayTips();
+})
